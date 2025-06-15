8ce3393f6f34bcc4194e1c420a49a72e6898c844
mikyasnega70 <mikenega70@gmail.com>
2025-06-15 14:43:03 -0700

    modify code

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,6 +2,7 @@
 class BankAccount:
     def __init__(self, account_balance):
         self.account_balance = 0
+        self.account_balance = account_balance
     
     def deposit(self, amount):
         self.account_balance += amount
